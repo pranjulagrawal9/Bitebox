1788ee4fc89997fb4ce3cbf9deca7f96d337e3d4
Pranjul Agrawal <pranjulagrawal9@gmail.com>
2023-04-30 17:03:41 +0530

    Add fallback in Suspense for Cart component

---
+++ src/components/CssLoader/CssLoader.js
@@ -0,0 +1,11 @@
+import './CssLoader.scss'
+
+function CssLoader() {
+  return (
+    <div className="loader-container">
+        <span class="loader"></span> 
+    </div>
+  )
+}
+
+export default CssLoader
--- src/index.js
+++ src/index.js
@@ -3,28 +3,34 @@ import App from "./App";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RestaurantInfo from "./components/RestaurantInfo/RestaurantInfo";
 import CardContainer from "./components/CardContainer/CardContainer";
-import Cart from "./components/Cart/Cart";
+import { lazy, Suspense } from "react";
+import CssLoader from "./components/CssLoader/CssLoader";
+const Cart = lazy(() => import("./components/Cart/Cart"));
 
-const router= createBrowserRouter([
-    {
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
         path: "/",
-        element: <App />,
-        children: [
-            {
-                path: "/",
-                element: <CardContainer />
-            },
-            {
-                path: "/restaurants/:restaurantURL",
-                element: <RestaurantInfo />
-            },
-            {
-                path: "/checkout",
-                element: <Cart />
-            }
-        ]
-    }
-])
+        element: <CardContainer />,
+      },
+      {
+        path: "/restaurants/:restaurantURL",
+        element: <RestaurantInfo />,
+      },
+      {
+        path: "/checkout",
+        element: (
+          <Suspense fallback={<CssLoader />}>
+            <Cart />
+          </Suspense>
+        )
+      }
+    ]
+  }
+]);
 
 const container = document.getElementById("root");
 const root = createRoot(container);
